5ea30180fa3af31dc72c362dec01a744e5b09318
kanakuro <tkykk.0914@gmail.com>
2024-10-03 18:28:49 +0900

    不要項目の削除

--- app/api.py
+++ app/api.py
@@ -3,7 +3,6 @@ import urllib.parse
 
 headers = {
     "accept": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4M2YwNWJlMDJmMTgyZGM2MWJlM2QxN2QyMmMzMTljMyIsIm5iZiI6MTcyNzgzMDYzOC4wNDI5NDMsInN1YiI6IjY2Zjc0Zjg4ZTdkMjRlYmIyYmExZTU5MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YOZRYiYWWvUEgUdmWBvORhmN3gy1ZUQZge4Am83tTAo"
 }
 
 def get_movie_image_url(movie_title):
